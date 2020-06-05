c4968b634b2b120f202683a1b61f6c8a4c93103d
Yiling Qiao <yqiao@yqiao-mac01.local>
2020-06-05 10:52:46 -0400

    read .ply using open 3d

--- example/script_inference.py
+++ example/script_inference.py
@@ -1,11 +1,20 @@
 # import open3d as o3d
 import torch
+import open3d as o3d
+import numpy as np
 
 from ml3d import Predictor
 from ml3d.util import Config
 
+
 config_file     = 'ml3d/config/semantic_segmentation/randlanet_semantickitti.py'
 checkpoint_file = 'ml3d/checkpoint/randlanet_semantickitti.pth'
+pointcloud_file = 'datasets/fragment.ply'
+
+pcd = o3d.io.read_point_cloud(pointcloud_file)
+np_input    = np.concatenate((np.asarray(pcd.points), 
+                    np.asarray(pcd.colors)), axis=1).astype(np.float32)
+
 
 
 cfg         = Config.load_from_file(config_file)
@@ -14,9 +23,11 @@ device      = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 predictor   = Predictor(cfg, checkpoint_file, device=device)
 
-#data        = o3d.io.read_point_cloud(pointcloud_file)
-data        = 1000*torch.randn(1, 2**16, 7).to(device)
+data        = torch.from_numpy(np_input).unsqueeze(0).to(device)
+#data        = 1000*torch.randn(1, 2**16, 7).to(device)
 
 result      = predictor(data)
+
+o3d.visualization.draw_geometries([pcd])
 print(result.size())
 #o3d.visualization.draw()
--- ml3d/config/semantic_segmentation/randlanet_semantickitti.py
+++ ml3d/config/semantic_segmentation/randlanet_semantickitti.py
@@ -1,27 +1,11 @@
 # model settings
 network = dict(
     type='RandLANet',
-    d_in=7,
+    d_in=6,
     num_classes=6
     )
 # model training and testing settings
 train_cfg = dict(
-    rcnn=dict(
-        assigner=dict(
-            type='MaxIoUAssigner',
-            pos_iou_thr=0.5,
-            neg_iou_thr=0.5,
-            min_pos_iou=0.5,
-            match_low_quality=False,
-            ignore_iof_thr=-1),
-        sampler=dict(
-            type='RandomSampler',
-            num=512,
-            pos_fraction=0.25,
-            neg_pos_ub=-1,
-            add_gt_as_proposals=True),
-        pos_weight=-1,
-        debug=False))
+    )
 test_cfg = dict(
-    rcnn=dict(
-        score_thr=0.05, nms=dict(type='nms', iou_thr=0.5), max_per_img=100))
+    )
