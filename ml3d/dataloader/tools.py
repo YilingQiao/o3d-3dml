4342c22f5d4f92ff9768b86fc2b091a6156e7001
YilingQiao <yilingq@umd.edu>
2020-06-13 23:22:03 -0400

    train

--- ml3d/dataloader/tools.py
+++ ml3d/dataloader/tools.py
@@ -44,28 +44,6 @@ class DataProcessing:
         return data_list
 
     @staticmethod
-    def grid_sub_sampling(points, features=None, labels=None, grid_size=0.1, verbose=0):
-        """
-        CPP wrapper for a grid sub_sampling (method = barycenter for points and features
-        :param points: (N, 3) matrix of input points
-        :param features: optional (N, d) matrix of features (floating number)
-        :param labels: optional (N,) matrix of integer labels
-        :param grid_size: parameter defining the size of grid voxels
-        :param verbose: 1 to display
-        :return: sub_sampled points, with features and/or labels depending of the input
-        """
-
-        if (features is None) and (labels is None):
-            return cpp_subsampling.compute(points, sampleDl=grid_size, verbose=verbose)
-        elif labels is None:
-            return cpp_subsampling.compute(points, features=features, sampleDl=grid_size, verbose=verbose)
-        elif features is None:
-            return cpp_subsampling.compute(points, classes=labels, sampleDl=grid_size, verbose=verbose)
-        else:
-            return cpp_subsampling.compute(points, features=features, classes=labels, sampleDl=grid_size,
-                                           verbose=verbose)
-
-    @staticmethod
     def IoU_from_confusions(confusions):
         """
         Computes IoU from confusion matrices.
